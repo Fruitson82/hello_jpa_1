1060e620129827de65a97c4a70a727a56608ea37
fruitson <fruitson@gmail.com>
2020-09-08 23:45:06 +0900

    first commit

---
+++ src/main/java/hellojpa/JpaMain.java
@@ -0,0 +1,37 @@
+package hellojpa;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("hello");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+        tx.begin();
+
+        try {
+            // 비영속
+//            Member member = new Member();
+//            member.setId(100L);
+//            member.setName("HelloJPA");
+//
+//            // 객체를 저장한 상태(영속)
+//            em.persist(member);
+
+            tx.commit();
+        }catch(Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        }finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+
+}
---
+++ src/main/java/hellojpa/Member.java
@@ -0,0 +1,34 @@
+package hellojpa;
+
+import jdk.nashorn.internal.runtime.logging.Logger;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+public class Member {
+
+    @Id
+    private Long id;
+
+    @Column(name = "name")
+    private String username;
+
+    private Integer age;
+
+    @Enumerated(EnumType.STRING)
+    private RoleType roleType;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createDate;
+
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastModifiedDate;
+
+    @Lob
+    private String description;
+
+    public Member() {
+    }
+
+}
